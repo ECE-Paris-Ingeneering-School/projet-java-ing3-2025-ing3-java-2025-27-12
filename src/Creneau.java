66dc020ef81826695aa8de0e769df2c4a5aef87a
Stanislasleclerc <98821843+Stanislasleclerc@users.noreply.github.com>
2025-04-23 17:22:22 +0200

    Update Creneau.java

--- src/Creneau.java
+++ src/Creneau.java
@@ -7,31 +7,50 @@ public class Creneau {
     private boolean disponible;
     private String nomSpecialiste;
     private String prenomSpecialiste;
+
+
     private String specialisation;
+    private String ville; // ✅ Nouveau champ
 
 
     public Creneau(int id, int idSpecialiste, LocalDateTime dateHeure, boolean disponible,
                    String nom, String prenom, String specialisation) {
         this.id = id;
-        this.idSpecialiste = idSpecialiste;
-        this.dateHeure = dateHeure;
-        this.disponible = disponible;
-        this.nomSpecialiste = nom;
-        this.prenomSpecialiste = prenom;
-        this.specialisation = specialisation;
+
+        this.idSpecialiste
+                = idSpecialiste;
+        this.dateHeure
+                = dateHeure;
+        this.disponible
+                = disponible;
+        this.nomSpecialiste
+                = nom;
+        this.prenomSpecialiste
+                = prenom;
+        this.specialisation
+                = specialisation;
     }
-    public String getNomSpecialiste() { return nomSpecialiste; }
-    public String getPrenomSpecialiste() { return prenomSpecialiste; }
-    public String getSpecialisation() { return specialisation; }
 
 
     public int getId() { return id; }
-    public int getIdSpecialiste() { return idSpecialiste; }
+   // public int getIdSpecialiste() { return idSpecialiste; }
     public LocalDateTime getDateHeure() { return dateHeure; }
     public boolean isDisponible() { return disponible; }
+    public String getNomSpecialiste() { return nomSpecialiste; }
+    public String getPrenomSpecialiste() { return prenomSpecialiste; }
+    public String getSpecialisation() { return specialisation; }
+    public String getVille() { return ville; }
 
-    @Override
-    public String toString() {
-        return dateHeure.toString();
+
+    public void setVille(String ville) {
+
+
+        this.ville = ville;
     }
+
+            @Override
+            public String toString() {
+                return dateHeure.toString();
+            }
 }
+
