18ee22708c21ea850b69126406c620c4027d02c1
Stanislasleclerc <98821843+Stanislasleclerc@users.noreply.github.com>
2025-04-23 17:22:03 +0200

    Update ConnexionBDD.java

--- src/ConnexionBDD.java
+++ src/ConnexionBDD.java
@@ -1,5 +1,4 @@
-// ConnexionBDD.java
-// ConnexionBDD.java
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,11 +6,15 @@ import java.sql.*;
 
 public class ConnexionBDD {
     private static final String URL = "jdbc:mysql://localhost:3306/rendez_vous_specialiste";
-    private static final String USER = "root"; // à adapter
-    private static final String PASSWORD = "";  // à adapter
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
     public static Connection getConnexion() throws SQLException {
+
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
+
 }
 
