f3adc64d2e4f9024eb9aee1b1de15283618f09fd
Stanislasleclerc <98821843+Stanislasleclerc@users.noreply.github.com>
2025-04-23 17:52:14 +0200

    Update MainFrameJavaFX.java

--- src/MainFrameJavaFX.java
+++ src/MainFrameJavaFX.java
@@ -2,11 +2,13 @@ import javax.swing.*;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
+//import java.awt.*;
+//import java.sql.*;
 public class MainFrameJavaFX {
     public static void main(String[] args) {
 
 
         SwingUtilities.invokeLater(FenetreAccueil::new);
-//        javax.swing.SwingUtilities.invokeLater(() -> new FenetreConnexion());
+//        javax.swing.invokeLater(() -> new FenetreConnexion());
     }
 }
